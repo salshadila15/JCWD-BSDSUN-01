8a45b0d61459a78ddc6622f2546716a8dc9a0a32
salshadila15 <salshadilaherfiananda515@gmail.com>
2025-11-16 16:04:56 +0700

    if statement and looping

--- Module 1/Pertemuan ke 2/index.ts
+++ Module 1/Pertemuan ke 2/index.ts
@@ -1,1 +1,14 @@
 console.log("Hello, welcome to Module 1, Meeting 2!");
+
+// Create a function convertDate
+// Create a function convertDate
+function convertDate(date: Date): string {
+    return date.toISOString().split('T')[0];
+}
+console.log(convertDate(new Date()));
+
+
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+
